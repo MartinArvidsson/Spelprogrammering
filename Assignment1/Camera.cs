24c80a0f82dbacfd81f18bf56a206238bcb3e088
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2015-11-13 22:24:03 +0100

    1 of 4 done

---
+++ Assignment1/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Assignment1
+{
+    class Camera
+    {
+        int sizeOfTile = 64;
+        int borderSize = 64;
+
+        public Vector2 GetCoordinates(int xcoordinate,int ycoordinate)
+        {
+            int visualX = borderSize + xcoordinate * sizeOfTile;
+            int visualY = borderSize + ycoordinate * sizeOfTile;
+
+            return new Vector2(visualX, visualY);
+        }
+    }
+}
---
+++ Assignment1/Gamecontroller.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Assignment1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Gamecontroller : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D squareBlack;
+        Texture2D squareWhite;
+        Texture2D chessPiece;
+        Camera camera = new Camera();
+
+        public Gamecontroller()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 700;
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.ApplyChanges();
+            Content.RootDirectory = "Content";
+            graphics.IsFullScreen = false;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            squareBlack = Content.Load<Texture2D>("squareblack64.png");
+            squareWhite = Content.Load<Texture2D>("squarewhite64.png");
+            chessPiece = Content.Load<Texture2D>("Chesspiece.png");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            // TODO: Add your update logic here
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            // TODO: Add your drawing code here
+            int counter = 0;
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                   if(counter % 2 == 0)
+                   {
+                       //Rita vit
+                       spriteBatch.Draw(squareWhite,camera.GetCoordinates(x,y),Color.White);
+                   }
+                   else
+                   {
+                        //Rita svart
+                       spriteBatch.Draw(squareBlack,camera.GetCoordinates(x,y),Color.White);
+                   }
+                    counter ++;
+                }
+                counter ++;
+            }
+            spriteBatch.End();
+                base.Draw(gameTime);
+        }
+    }
+}
